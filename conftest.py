e385bddf03e54b43f64591de4cac20aab1926b15
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2025-11-23 21:46:07 +0500

    Chapter 3 complete. Officially

--- conftest.py
+++ conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        action='store',
+        default='chrome',
+        help="Choose browser: chrome or firefox")
     parser.addoption(
         '--language',
         action='store',
@@ -13,10 +19,21 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def browser(request):
     user_language = request.config.getoption("language")
-    options = Options()
-    options.add_argument(f'lang={user_language}')
-    browser = webdriver.Chrome(options=options)
-
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_argument(f'lang={user_language}')
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = OptionsFirefox()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
+    print("\nquit browser..")
     browser.quit()
 
--- test_items.py
+++ test_items.py
@@ -3,8 +3,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-link = 'https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
-def test_stepik_page(browser):
+link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+def test_button_add_to_basket(browser):
     browser.get(link)
     checked_button = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn-add-to-basket'))
